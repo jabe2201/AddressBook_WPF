95de40524db35e516be8cc4b4beea1db87b7cc9c
jabe2201 <jacobwback@gmail.com>
2022-10-12 20:36:24 +0200

    Added x:Name to Textboxes in MainWindow.xaml Also _contacts and _filepath

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +13,26 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Models;
 
 namespace WpfApp1
 {
     
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Contact> _contacts;
+        private string _filePath= $@"C:\Users\jacob\Documents\Nackademin\ProgrammeringC#\ovningar\Uppgift2_TEST\addressbook.json";
+
+
         public MainWindow()
         {
             InitializeComponent();
+            lv_Contacts.ItemsSource = _contacts;
+        }
+
+        private void bt_Add_Click(object sender, RoutedEventArgs e)
+        {
+            var contact = _contacts.Where(x => x.EmailAddress == tb_)
         }
     }
 }
